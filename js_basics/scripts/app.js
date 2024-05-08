85eb66b4835f2c721520e390586518cc758b8f01
Mike Soltysiak <mtsolt@outlook.com>
2024-05-08 11:02:49 +1000

    adds description of the reasons for the new functionality

--- js_basics/scripts/app.js
+++ js_basics/scripts/app.js
@@ -6,9 +6,14 @@ function init() {
 const button = document.getElementById('button1')
 const body = document.querySelector('body')
 
-
-
+// Creating the new element here doesn't work.
+// let newBox = document.createElement('p')
+// newBox.innerText = 'Clicked'
+// newBox.style.color = 'white'
+// This is because when creating the p outside the function, that p is the same node, and you cannot add the same node to a parent
+// multiple times.
 function buttonClick() {
+  // However having it inside the function does. Because each time the function is called it will create a new 
   let newBox = document.createElement('p')
   newBox.innerText = 'Clicked'
   newBox.style.color = 'white'
